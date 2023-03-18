592bdccfe321515bce109c0698d509695f541680
WK <wk_suspect0318@outlook.com>
2023-03-18 13:17:11 +0800

    18/3/2023 13:16 OK

--- answer.cpp
+++ answer.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
+    int i,n,o=0;
+    cout<<"enter an long int"<<endl;
+    cin>>i;
+    while (i){
+        n=i%10;i/=10;
+        o=o*10+n;
 
-    cout<<"hello world"<<endl;
+    }
+
+    cout<<o<<endl;
     return 0;
 }
 

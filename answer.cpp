ccccdfd25f0ce7cbf7b8f137713bea60007d57aa
WK <wk_suspect0318@outlook.com>
2023-03-19 16:00:57 +0800

    19/3/2023 16:00 1.6 OK ending

--- answer.cpp
+++ answer.cpp
@@ -4,23 +4,12 @@ using namespace std;
 
 
 int main(){
-    int n;
-    char c;
-    cout<<"enter an number(height of pyramid) : ";
-    cin>>n;
-    cout<<endl<<"enter a char : ";
-    cin>>c;
-    cout<<endl;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){cout<<" ";}
-        for (int k=0;k<i;k++)
-        cout<<c<<" ";
-        cout<<endl;
-    }
-
-
-
-
+    double a,b,c,ans,p;
+    cout<<"enter the score of the student: ";
+    cin>>a>>b>>c;
+    ans=a+b+c;
+    p=ans/3.0;
+    printf("%.2f %.2lf",ans,p);
     return 0;
 }
 

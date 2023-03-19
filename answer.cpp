e8fc987c0ef56e0311ea39e98fb3a0c174f17eb6
WK <wk_suspect0318@outlook.com>
2023-03-19 08:19:00 +0800

    19/3/2023 8:16 1.4 OK ending

--- answer.cpp
+++ answer.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 
 int main(){
-    char *ch,c;
-    string str;
-    ch=new char[100];
-    int x='A'-'a',n=0;
-    while(cin>>c){
-        //if(!(c>='A'&&c<='Z')) break;
-        cout<<(char)(c+32)<<endl;
-        //cin>>c;
+    double v,r;
+    const double pai=3.1415926;
+    cout<<"Radius of a sphere: "<<endl;
+    cin>>r;
+    v=4.0*r*r*r*pai/3;//ps .'4/3*r*r*r*pai' is wrong!!!!! should (4/3)*r*r*r*pai
+    cout<<v<<endl;
+    printf("volume of sphere : %.4f\n",v);
 
-        //ch[n++]=c;
-        //cin>>ch[n++];
-
-        //if (cin.get()=='\n') break;//********important judge "enter"*****---
-    }
-//    for (int i=0;i<str.size();i++)
-//        if(str[i]!='\n')
-//        cout<<(char)(ch[i]+32)<<endl;
 
 
     return 0;

6780ccf33fe8979c71b88fdfd86b6201dd6975da
WK <wk_suspect0318@outlook.com>
2023-03-19 20:55:07 +0800

    19/3/2023 20:54 2.1 OK ending

--- answer.cpp
+++ answer.cpp
@@ -5,18 +5,16 @@ using namespace std;
 
 
 int main(){
-    int n,m,h;
-    cout<<"the number of milk left over :n , the speed of drinking milk(minutes) :h , passing time : m. (n h m)"<<endl;
-    cin>>n>>h>>m;
-    while (n*h<m){
-        cout<<"Time error!! (n*m>h),please enter again"<<endl;
-        cin>>n>>h>>m;
+    int score;
+    while (1)
+    {
+        cout<<"enter a score: (if you want out ,enter -1)"<<endl;
+        cin>>score;
+        if (score==-1) break;
+        if(score>=90&& score<=100) cout<<"perfect"<<endl;
+        system("pause");
+
     }
-    int x=0;
-    x=m/h;
-    if(x*h<m) ++x;
-    x=n-x;
-    cout<<"last milk : "<<x<<endl;
 
     return 0;
 }

69ae9c2886939a3b0545d309aefe9552b3c9cd5e
WK <wk_suspect0318@outlook.com>
2023-03-19 20:13:50 +0800

    19/3/2023 20:13 1.10 OK ending

--- answer.cpp
+++ answer.cpp
@@ -2,23 +2,21 @@
 #include <string>
 #include <math.h>
 using namespace std;
-//Determine whether three sides from a triangle
-//判断三条边是否构成三角形
-bool judgeIS(int a,int b, int c){
-    if(a+b>c && a+c>b && c+b>a && abs(a-b)<c && abs(a-c)<b && abs(b-c)<a)
-        return true;
-    cout<<"It is not a triangle , please enter three edge again!"<<endl;
-    return false;
-}
+
 
 int main(){
-    int a,b,c;
-    double circle,area,p;
-    do {cin>>a>>b>>c;} while (!judgeIS( a,b,c));
-    circle=a+b+c;
-    p=0.5*circle;
-    area=sqrt(p*(p-a)*(p-b)*(p-c));
-    printf("circumference=%.2lf area=%.2lf \n",circle,area);
+    int n,m,h;
+    cout<<"the number of milk left over :n , the speed of drinking milk(minutes) :h , passing time : m. (n h m)"<<endl;
+    cin>>n>>h>>m;
+    while (n*h<m){
+        cout<<"Time error!! (n*m>h),please enter again"<<endl;
+        cin>>n>>h>>m;
+    }
+    int x=0;
+    x=m/h;
+    if(x*h<m) ++x;
+    x=n-x;
+    cout<<"last milk : "<<x<<endl;
 
     return 0;
 }

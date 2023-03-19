48cb81af7bb6704326119096cbab3c515929f986
WK <wk_suspect0318@outlook.com>
2023-03-19 19:31:52 +0800

    19/3/2023 19:31 1.9 OK ending

--- answer.cpp
+++ answer.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
-
+//Determine whether three sides from a triangle
+//判断三条边是否构成三角形
+bool judgeIS(int a,int b, int c){
+    if(a+b>c && a+c>b && c+b>a && abs(a-b)<c && abs(a-c)<b && abs(b-c)<a)
+        return true;
+    cout<<"It is not a triangle , please enter three edge again!"<<endl;
+    return false;
+}
 
 int main(){
-    int a,n;
-    cout<<"you will exit ,if you enter char "<<endl;
-    while(cin>>n){
-        a=1;
-        a<<=n;
-        cout<<a<<endl;
-    }
+    int a,b,c;
+    double circle,area,p;
+    do {cin>>a>>b>>c;} while (!judgeIS( a,b,c));
+    circle=a+b+c;
+    p=0.5*circle;
+    area=sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("circumference=%.2lf area=%.2lf \n",circle,area);
+
     return 0;
 }
 

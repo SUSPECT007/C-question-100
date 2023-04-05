78edb97c25ccbcd1e3cb1e78d832d2dc9e30a344
WK <wk_suspect0318@outlook.com>
2023-04-05 21:01:16 +0800

    5/4/2023 21:01 2.3 OK ending

--- answer.cpp
+++ answer.cpp
@@ -4,25 +4,25 @@
 #include <vector>
 using namespace std;
 
-int gcd(int a ,int b){
-    int temp=a%b;
-    while(temp){
-        a=b;b=temp;
-        temp=a%b;
-    }
-    return b;
 
-}
 
 
 int main(){
+    char c;
+    int A=0,B=0;
+    while(cin>>c){
 
-  cout<<"enter two int a,b "<<endl;
-  int a,b,greatest,least;
-  cin>>a>>b;
-  least=gcd(a,b);
-  greatest=a*b/least;
-  cout<<"greatest common divisor: "<<greatest<<" least common multiple: "<<least<<endl;
+        if (c=='0') break;
+        //cin>>c;
+        if(c=='A') ++A;
+        else if(c=='B') ++B;
+        //else cout<<"enter error !!! please enter A OR B!!!"<<endl;
+    }
+    if(A==B) cout<<endl<<"E"<<endl;
+    else if(A>B) cout<<endl<<"A"<<endl;
+    else  cout<<endl<<"B"<<endl;
+
+
 
     return 0;
 }

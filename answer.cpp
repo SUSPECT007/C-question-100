9be191c1ef00b2b9f556527c876a744e1c312556
WK <wk_suspect0318@outlook.com>
2023-04-13 22:41:42 +0800

    13/4/2023 22:41 2.4 OK ending

--- answer.cpp
+++ answer.cpp
@@ -2,16 +2,33 @@
 #include <string>
 #include <math.h>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 
 
 
 int main(){
-   double a,b,c,deta;
+   double a,b,c,deta,x1,x2;
    cin>>a>>b>>c;
+   cout<<setiosflags(ios::fixed)<<setprecision(2);
    if(a==0) {cout<<endl<<"Not quadratic equation"<<endl; return 0;}
    deta=b*b-4*a*c;
+   if (deta==0){
+       x1=(-b)/(2.0*a);
+       cout<<"x1=x2="<<x1<<endl;
+   }
+    if (deta>0){
+        double d=sqrt(deta);
+        x1=(-b+d)/(2.0*a);
+        x2=(-b-d)/(2.0*a);
+        cout<<"x1="<<x1<<"x2="<<x2<<endl;
+    }
+    if (deta<0){
+        double i=sqrt(-deta)/(2*a);
+        double r=(-b)/(2.0*a);
+        cout<<"x1="<<r<<" +"<<i<<"i "<<"x2="<<r<<" -"<<i<<"i "<<endl;
+    }
 
 
 
